f6307324968b46ff3d5e6b386ca7a213865ef8b3
rinaldic <71988764+rinaldic@users.noreply.github.com>
2023-01-13 15:08:43 -0300

    Update index.js

--- proyecto-grupal/client/src/index.js
+++ proyecto-grupal/client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-//import ReactDOM from 'react-dom/client';  para react-dom ver 18 o superior
+//import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';  //para react-dom ver 18 o superior
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,7 +8,7 @@ import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Auth0Provider } from "@auth0/auth0-react";
 import {store} from './redux/store';
-import axios from "axios";
+//import axios from "axios";
 
 // Configuracion Auth0
 console.log(
@@ -20,12 +20,11 @@ const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const postLoginLocal = 'http://localhost:3000/postlogin';
 const postLoginDeploy = 'https://proyecto-grupal3.vercel.app/postLogin';
 
-//axios.defaults.baseURL = 'http://localhost:3001'
-//axios.defaults.baseURL = 'proyecto-grupal-back-production.up.railway.app/'
+
+//  ReactDOM.render(  // esta va con react v17, con v18 va lo de arriba
 // con react-dom v18 y superior
-//const root = ReactDOM.createRoot(document.getElementById('root')); 
-//root.render(
-  ReactDOM.render(  // esta va con react v17, con v18 va lo de arriba
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(
  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
@@ -35,7 +34,7 @@ const postLoginDeploy = 'https://proyecto-grupal3.vercel.app/postLogin';
       </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root') // esta linea va si se usa react-dom ver 17, con la 18 no va
+  //document.getElementById('root') // esta linea va si se usa react-dom ver 17, con la 18 no va
 );
 
 // If you want to start measuring performance in your app, pass a function
